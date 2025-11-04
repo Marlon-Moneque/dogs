573298281785abeff534585c4a6e13b65ef6d751
Marlon Moneque <marlonmoneque@gmail.com>
2025-11-03 22:45:35 -0300

    link

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import './App.css'
 import { UserStorage } from './UserContext';
@@ -15,23 +15,23 @@ import NotFound from './components/NotFound';
 const App = () => {
   return (
   <div className='App'>
-    <HashRouter>
+    <BrowserRouter>
       <UserStorage>
           <Header />
           <main className='AppBody'>
             <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='login/*' element={<Login />} />
-          <Route path='conta/*' 
-                element={<ProtectedRouter><User /></ProtectedRouter>} />
-          <Route path='foto/:id' element={<Photo />} />
-          <Route path='perfil/:user' element={<UserProfile />} />
-          <Route path='*' element={<NotFound />} />
+              <Route path='/' element={<Home />} />
+              <Route path='login/*' element={<Login />} />
+              <Route path='conta/*' 
+                    element={<ProtectedRouter><User /></ProtectedRouter>} />
+              <Route path='foto/:id' element={<Photo />} />
+              <Route path='perfil/:user' element={<UserProfile />} />
+              <Route path='*' element={<NotFound />} />
             </Routes>
           </main>
           <Footer />
         </UserStorage>
-    </HashRouter>
+    </BrowserRouter>
   </div>)
 };
 
