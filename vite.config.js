bb9b39ac90e5d690c4a45ccfd4d405a3ea727873
Marlon Moneque <marlonmoneque@gmail.com>
2025-11-03 23:35:32 -0300

    dist

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,4 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-    optimizeDeps: {
-    include: [] // Tente limpar ou ajustar esta configuração
-  },
-  build: {
-    rollupOptions: {
-      // Configurações específicas do Rollup se necessário
-    }
-  }
 });
